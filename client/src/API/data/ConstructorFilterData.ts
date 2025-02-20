150bab67d63f1dde7ad163f2261168239aeed178
KM <itmuminoff@gmail.com>
2025-02-20 15:35:43 +0500

    fx

---
+++ client/src/API/data/ConstructorFilterData.ts
@@ -0,0 +1,10 @@
+export interface ConstructorFilterScheme {
+	id: number;
+	title: string;
+}
+
+export const ConstructorFilterData: ConstructorFilterScheme[] = [
+	{ id: 1, title: "Трудовой договор" },
+	{ id: 2, title: "Трудовой договор" },
+	{ id: 3, title: "Трудовой договор" },
+];
--- client/src/Components/Filter/Filter.tsx
+++ client/src/Components/Filter/Filter.tsx
@@ -1,18 +1,44 @@
-import React from "react";
 import "./Filter.css";
 import { DataFilterType } from "../../API/data/dataFilter";
 import FilterElement from "./FilterElement/FilterElement";
+import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import {
+	ConstructorFilterData,
+	ConstructorFilterScheme,
+} from "../../API/data/ConstructorFilterData";
 
 interface TProps {
 	data: DataFilterType[];
 }
 
-const Filter: React.FC<TProps> = ({ data }: TProps) => {
+const Filter = ({ data }: TProps) => {
+	// useLocation
+	const location = useLocation();
+
+	const [filter, setFilter] = useState<boolean>(false);
+	useEffect(() => {
+		if (location.pathname === "/modules/hr/create") {
+			setFilter(true);
+		} else {
+			setFilter(false);
+		}
+	}, [location.pathname]);
 	return (
 		<ul className="wrapper-filter">
-			{data.map((e) => {
-				return <FilterElement item={e} />;
-			})}
+			{filter ? (
+				<>
+					{ConstructorFilterData.map((item) => (
+						<FilterElement item={item} />
+					))}
+				</>
+			) : (
+				<>
+					{data.map((e) => {
+						return <FilterElement item={e} />;
+					})}
+				</>
+			)}
 		</ul>
 	);
 };
