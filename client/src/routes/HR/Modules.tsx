b147cac9d8b0700118429e98cef2878c8270a4ea
AMIRTJK <jsharipovamir@gmail.com>
2025-02-24 14:34:14 +0500

    fx order modules index

--- client/src/routes/HR/Modules.tsx
+++ client/src/routes/HR/Modules.tsx
@@ -38,13 +38,13 @@ const Modules: React.FC<ModulesListScheme> = () => {
     {
       id: 3,
       title: "Первичые документы",
-      image: submoduleImage,
+      image: notFound,
       link: "/primary-docs/contracts",
     },
     {
       id: 4,
       title: "Кадр",
-      image: notFound,
+      image: submoduleImage,
       link: "/modules/hr/submodules",
     },
     {
