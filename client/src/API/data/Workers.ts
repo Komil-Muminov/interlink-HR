9ad3940276dea980e73e420566e8a1bffcac2305
KM <itmuminoff@gmail.com>
2025-02-24 18:16:52 +0500

    fx

--- client/src/API/data/Workers.ts
+++ client/src/API/data/Workers.ts
@@ -9,7 +9,22 @@ export interface IWorker {
 	org: string;
 	email: string;
 	citizenship: string;
-	status: string;
+	workerStatus: string;
+	living: string;
+	inn: string;
+	passportId: string;
+	educationLevel: string;
+	specialization: string;
+	eductionEndingYear: string;
+	university: string;
+	prevJob: string;
+	prevJobPeriod: string;
+	prevJobPositon: string;
+	prevJobStatus: string;
+	currJobStatus: string;
+	currJob: string;
+	currJobPosition: string;
+	currJobPeriod: string;
 }
 
 export const WorkerData: IWorker[] = [
@@ -24,19 +39,49 @@ export const WorkerData: IWorker[] = [
 		email: "test@gmail.com",
 		position: "Специалист",
 		citizenship: "Таджикистан",
-		status: "Активный",
+		living: "г.Душанбе",
+		workerStatus: "Активный",
+		inn: "12345678",
+		passportId: "A123456",
+		educationLevel: "Высшее",
+		specialization: "IT",
+		eductionEndingYear: "2019",
+		university: "Университет",
+		prevJob: "OOO Технопарк",
+		prevJobPeriod: "2021",
+		prevJobPositon: "Чойдамкнак",
+		prevJobStatus: "Завершенная",
+		currJobStatus: "Текущая",
+		currJob: 'ГУП "ЦФИТ"',
+		currJobPeriod: "2022- настоящее время",
+		currJobPosition: "Специалист",
 	},
 	{
 		id: 2,
 		fname: "Табаров Комил",
 		tel: "+9919999991306",
-		bDate: "12.12.1997",
+		bDate: "01.04.1997",
 		citizenship: "Таджикистан",
 		org: `ГУП "ЦФИТ"`,
-		status: "Активный",
+		workerStatus: "Активный",
 		experience: "3 года, 2 месяца",
 		email: "test@gmail.com",
 		fExperience: "3 года, 2 месяца",
 		position: "Специалист",
+		living: "г.Душанбе",
+		inn: "12345678",
+		passportId: "A123456",
+		educationLevel: "Высшее",
+		specialization: "IT",
+		university: "Университет",
+		eductionEndingYear: "2019",
+		prevJob: "OOO Технопарк",
+		prevJobPeriod: "2021",
+		prevJobPositon: "Чойдамкнак",
+		prevJobStatus: "Завершенная",
+		currJobStatus: "Текущая",
+		currJob: 'ГУП "ЦФИТ"',
+		currJobPeriod: "2022- настоящее время",
+		currJobPosition: "Специалист",
 	},
 ];
--- client/src/UI/Worker-exprerience/WorkerExperience.tsx
+++ client/src/UI/Worker-exprerience/WorkerExperience.tsx
@@ -1,59 +1,137 @@
 import { Form } from "../Form/Form";
 import TitleSection from "../Title of Section/TitleSection";
 import { useConfirm } from "../../API/hooks/useConfirm";
+import { WorkerData } from "../../API/data/Workers";
+import "../Worker-personal/WorkerPersolan.css";
+import "./WorkerExperience.css";
 
-const WorkerExprerience: React.FC<{ [key: string]: string | undefined }> = ({
-	expreince,
-}) => {
-	const { handleToggle, toggle } = useConfirm();
+const WorkerExprerience: React.FC<{ worker: string }> = ({ worker }) => {
+	const validWorker = WorkerData.find((item) => item.fname === worker.name);
 	return (
 		<div className="worker-content worker__experience">
 			<TitleSection title="Профессиональная информация" />
-			<div>Опыт работы</div>
+			<h2 className="worker__education-title">Образование</h2>
 			<Form
-				label={"km"}
 				inputs={[
-					{ name: expreince.name },
 					{
-						name: "job",
-						label: "Статус работы",
-						type: "select",
-						classname: "createWorker-inp",
-						options: ["Текущая", "Министерство юстиции"],
+						name: "educationLevel",
+						value: validWorker?.educationLevel,
+						label: "Уровень образование",
+						disabled: true,
+						classname: "workerPersonal-inp",
 					},
+
 					{
-						name: "job",
-						label: "Место работы",
-						type: "select",
-						classname: "createWorker-inp",
-						options: ["Текущая", "Министерство юстиции"],
+						name: "educationSpecialization",
+						value: validWorker?.specialization,
+						classname: "workerPersonal-inp",
+						disabled: true,
+						label: "Специальность",
 					},
+
 					{
-						name: "job",
-						label: "Должность",
-						type: "select",
-						classname: "createWorker-inp",
-						options: ["Текущая", "Министерство юстиции"],
+						name: "educationUniversity",
+						value: validWorker?.university,
+						classname: "workerPersonal-inp",
+						disabled: true,
+						label: "Учебное заведение",
 					},
+
 					{
-						name: "job",
-						label: "Период с",
-						type: "select",
-						classname: "createWorker-inp",
-						options: ["Текущая", "Министерство юстиции"],
+						name: "educationEndingYear",
+						value: validWorker?.eductionEndingYear,
+						label: "Год окончание",
+						disabled: true,
+						classname: "workerPersonal-inp",
 					},
-					{
-						name: "job",
-						label: "Период до",
-						type: "select",
-						classname: "createWorker-inp",
-						options: ["Текущая", "Министерство юстиции"],
-					},
-					{ name: "worker-file", type: "file" },
 				]}
+				classname="workerPersonal-form"
 			/>
-			<button onClick={handleToggle}>confirm</button>
-			{/* {toggle && <Worker} */}
+
+			<div className="workerExperience__card-content">
+				<div className="workerExperience-card">
+					<h3 className="work__experience-title">Опыт работы</h3>
+					<Form
+						inputs={[
+							{
+								name: "currJobStatus",
+								value: validWorker?.currJobStatus,
+								classname: "workerPersonal-inp",
+								disabled: true,
+								label: "Статус работы",
+							},
+
+							{
+								name: "currJob",
+								value: validWorker?.currJob,
+								classname: "workerPersonal-inp",
+								disabled: true,
+								label: "Место работы",
+							},
+
+							{
+								name: "currJobPosition",
+								classname: "workerPersonal-inp",
+								value: validWorker?.currJobPosition,
+								disabled: true,
+								label: "Должность",
+							},
+
+							{
+								name: "workStatus",
+								value: validWorker?.currJobPeriod,
+								disabled: true,
+								classname: "workerPersonal-inp",
+								label: "Период",
+							},
+						]}
+						classname="workerPersonal-form workerExperience-form"
+					/>
+				</div>
+
+				<div className="workerExperience-card">
+					<h3 className="prevjob-title">PreJob</h3>
+					<Form
+						inputs={[
+							{
+								name: "PrevJobStatus",
+								value: validWorker?.prevJobStatus,
+								disabled: true,
+								classname: "workerPersonal-inp",
+								label: "Статус работы",
+							},
+							{
+								name: "prevJob",
+								value: validWorker?.prevJob,
+								classname: "workerPersonal-inp",
+								disabled: true,
+								label: "Место работы",
+							},
+							{
+								name: "prevJobPosition",
+								value: validWorker?.prevJobPositon,
+								disabled: true,
+								classname: "workerPersonal-inp",
+								label: "Должность",
+							},
+							{
+								name: "prevJobPeriod",
+								value: validWorker?.prevJobPeriod,
+								label: "Период",
+								classname: "workerPersonal-inp",
+								disabled: true,
+							},
+						]}
+						classname="workerPersonal-form workerExperience-form"
+					/>
+				</div>
+				<div className="work-eperience-file">
+					<label htmlFor="fileInput" className="file-label">
+						+
+						<input type="file" id="fileInput" className="file-input" />
+					</label>
+				</div>
+			</div>
 		</div>
 	);
 };
