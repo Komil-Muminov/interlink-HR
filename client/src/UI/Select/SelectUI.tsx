7fa22ccbde80dbff721810503588ec4a40cdfe7a
AMIRTJK <jsharipovamir@gmail.com>
2025-02-25 19:50:18 +0500

    create employment contract in proccess

--- client/src/UI/Select/SelectUI.tsx
+++ client/src/UI/Select/SelectUI.tsx
@@ -3,62 +3,70 @@ import "./SelectUI.css";
 import { Box, FormControl, InputLabel, MenuItem } from "@mui/material";
 import { Controller } from "react-hook-form";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { TUsers } from "../../API/data/Users";
 
 interface TData {
-	id: number;
-	title: string;
+  id: number;
+  title: string;
 }
 
 interface TProps {
-	disable: boolean;
-	control: any;
-	nameValue: string;
-	labelValue: string;
-	borderRadiusStyle: string;
-	heightStyle?: string;
-	widthStyle: string;
-	data: TData[];
+  disable: boolean;
+  control: any;
+  nameValue: string;
+  labelValue: string;
+  borderRadiusStyle: string;
+  heightStyle?: string;
+  widthStyle: string;
+  data: TData[] | TUsers[];
 }
 
 const SelectUI = ({
-	control,
-	disable,
-	nameValue,
-	labelValue,
-	borderRadiusStyle,
-	widthStyle,
-	data,
+  control,
+  disable,
+  nameValue,
+  labelValue,
+  borderRadiusStyle,
+  widthStyle,
+  data,
 }: TProps) => {
-	return (
-		<Box width={widthStyle}>
-			<Controller
-				name={nameValue}
-				control={control}
-				defaultValue=""
-				render={({ field }) => (
-					<FormControl fullWidth>
-						<InputLabel id="terCode-label">{labelValue}</InputLabel>
-						<Select
-							{...field}
-							id={nameValue}
-							disabled={disable}
-							labelId={`${nameValue}-label`}
-							label={labelValue}
-							sx={{
-								"& .MuiOutlinedInput-notchedOutline": {
-									borderRadius: borderRadiusStyle,
-								},
-							}}
-						>
-							{data.map((e) => {
-								return <MenuItem value={e.title}>{e.title}</MenuItem>;
-							})}
-						</Select>
-					</FormControl>
-				)}
-			/>
-		</Box>
-	);
+  return (
+    <Box width={widthStyle}>
+      <Controller
+        name={nameValue}
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <FormControl fullWidth>
+            <InputLabel id="terCode-label">{labelValue}</InputLabel>
+            <Select
+              {...field}
+              id={nameValue}
+              disabled={disable}
+              labelId={`${nameValue}-label`}
+              label={labelValue}
+              sx={{
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderRadius: borderRadiusStyle,
+                },
+              }}
+            >
+              {data.map((e) => {
+                return (
+                  <MenuItem
+                    key={e.id}
+                    value={"title" in e ? e.title : e.fullname}
+                  >
+                    {"fullname" in e ? e.fullname : e.title}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        )}
+      />
+    </Box>
+  );
 };
 
 export default SelectUI;
--- client/src/routes/HR/Personnel Records Management/Create/CreatePersonnelRecordsManagement.tsx
+++ client/src/routes/HR/Personnel Records Management/Create/CreatePersonnelRecordsManagement.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CreatePersonnelRecordsManagement.css";
 import TitleSection from "../../../../UI/Title of Section/TitleSection";
 import PanelControl from "../../../../UI/Panel Control/PanelControl";
+import { documentsList } from "../../../../API/data/documentsList";
+import { Button } from "@mui/material";
+import Input from "../../../../UI/Input/Input";
+import { useForm } from "react-hook-form";
+import SelectUI from "../../../../UI/Select/SelectUI";
+import { users } from "../../../../API/data/Users";
 
 const CreatePersonnelRecordsManagement = () => {
+  const { register, watch, control, handleSubmit, setValue, getValues } =
+    useForm({
+      defaultValues: {
+        fullname: "",
+        role: "",
+        subdivision: "",
+        orgName: `ГУП "Центр финансовых информационных технологий"`,
+        phone: "",
+        email: "",
+        status: "Активный",
+        userType: "Новый",
+      },
+    });
+
+  const userType = watch("userType");
+  const chooseUser = watch("fullname");
+
+  const currentUser = users.find((e) => e.fullname === chooseUser);
+
+  // Используйте useEffect для обновления значений формы при изменении fullname
+  useEffect(() => {
+    if (userType === "Новый") {
+      // Если выбран "Новый", очищаем fullname и другие поля
+      setValue("fullname", "");
+      setValue("role", "");
+      setValue("subdivision", "");
+      setValue("phone", "");
+      setValue("email", "");
+    } else if (currentUser) {
+      // Если выбран существующий пользователь и он найден, устанавливаем данные
+      setValue("role", currentUser.role);
+      setValue("subdivision", currentUser.subdivision);
+      setValue("phone", currentUser.phone);
+      setValue("email", currentUser.email);
+    }
+  }, [userType, currentUser, setValue]);
+
+  const [activeTab, setActiveTab] = useState<string>(documentsList[0].item);
+
   return (
     <main className="create-personnel-records-management">
       <TitleSection title="Новый документ" />
@@ -12,11 +57,129 @@ const CreatePersonnelRecordsManagement = () => {
       //   saveButtonState={!isValidInn ? true : false}
       // editButtonState
       />
-      <section></section>
-      <TitleSection title="Данные пользователя" />
-      <section></section>
-      <TitleSection title="Трудовой договор" />
-      <section></section>
+      <section>
+        <div className="wrapper-nav-documents-list">
+          {documentsList.map((tab) => (
+            <Button
+              onClick={() => setActiveTab(tab.item)}
+              className={activeTab === tab.item ? "active-tab" : ""}
+              key={tab.id}
+              variant={activeTab === tab.item ? "contained" : "outlined"}
+            >
+              {tab.item}
+            </Button>
+          ))}
+        </div>
+      </section>
+      {/* Трудовой договор */}
+      {activeTab === "Трудовой договор" && (
+        <>
+          <TitleSection title="Данные пользователя" />
+          <section>
+            <form>
+              <SelectUI
+                control={control}
+                nameValue="userType"
+                labelValue="Тип пользователя"
+                borderRadiusStyle="30px"
+                widthStyle="100%"
+                data={[
+                  {
+                    id: 1,
+                    title: "Новый",
+                  },
+                  { id: 2, title: "Существующий" },
+                ]}
+              />
+              {userType === "Новый" && (
+                <Input
+                  register={register}
+                  classname="crtPrimaryDocs__form--isDataSuccess"
+                  idValue="fullname"
+                  labelValue="ФИО *"
+                  borderRadiusStyle="30px"
+                  heightStyle="90%"
+                  widthStyle="49%"
+                />
+              )}
+              {userType === "Существующий" && (
+                <SelectUI
+                  control={control}
+                  nameValue="fullname"
+                  labelValue="ФИО *"
+                  borderRadiusStyle="30px"
+                  widthStyle="49%"
+                  data={users}
+                />
+              )}
+
+              <Input
+                register={register}
+                classname="crtPrimaryDocs__form--isDataSuccess"
+                idValue="role"
+                labelValue="Должность *"
+                borderRadiusStyle="30px"
+                heightStyle="90%"
+                widthStyle="49%"
+              />
+              <SelectUI
+                control={control}
+                nameValue="subdivision"
+                labelValue="Подразделение *"
+                borderRadiusStyle="30px"
+                widthStyle="49%"
+                data={
+                  currentUser
+                    ? [{ id: 1, title: currentUser.subdivision }]
+                    : [
+                        {
+                          id: 1,
+                          title:
+                            "Отдел разработки и внедрения информационных программ",
+                        },
+                        { id: 2, title: "Отдел по работе с клиентами" },
+                        { id: 3, title: "Отдел кадров" },
+                        { id: 3, title: `ГУП "ЦФИТ"` },
+                      ]
+                }
+              />
+              <Input
+                register={register}
+                classname="crtPrimaryDocs__form--isDataSuccess"
+                idValue="orgName"
+                labelValue="Организация *"
+                borderRadiusStyle="30px"
+                heightStyle="90%"
+                widthStyle="49%"
+                disabled={true}
+              />
+              <Input
+                register={register}
+                classname="crtPrimaryDocs__form--isDataSuccess"
+                idValue="phone"
+                labelValue="Номер телефона *"
+                borderRadiusStyle="30px"
+                heightStyle="90%"
+                widthStyle="49%"
+              />
+              <Input
+                register={register}
+                classname="crtPrimaryDocs__form--isDataSuccess"
+                idValue="email"
+                labelValue="Электронная почта *"
+                borderRadiusStyle="30px"
+                heightStyle="90%"
+                widthStyle="49%"
+              />
+            </form>
+          </section>
+          <TitleSection title="Трудовой договор" />
+          <section></section>
+        </>
+      )}
+
+      {/* Другие документы */}
+      {activeTab !== "Трудовой договор" && <p>На этапе разработки...</p>}
     </main>
   );
 };
