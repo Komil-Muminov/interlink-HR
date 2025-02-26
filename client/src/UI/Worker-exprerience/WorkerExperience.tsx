c2259cc721ac983a72dae820052e8f3e6b1c4aad
KM <itmuminoff@gmail.com>
2025-02-26 18:38:02 +0500

    fx

--- client/src/UI/Worker-exprerience/WorkerExperience.tsx
+++ client/src/UI/Worker-exprerience/WorkerExperience.tsx
@@ -17,74 +17,56 @@ const WorkerExperience: React.FC<WorkerExperienceProps> = ({
 	worker,
 	approve,
 }) => {
-	// Найти работника по имени
 	const validWorker = WorkerData.find((item) => item.fname === worker);
 
 	// Состояния для загрузки файлов
 	const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
 	const [uploadStatus, setUploadStatus] = useState("");
 
-	// Обработчик изменения файлов
 	const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		if (e.target.files) {
 			const newArr = [...e.target.files].map((item) => ({
 				...item,
 				id: Date.now() + Math.random(),
 			}));
-			setSelectedFiles((prev) => {
-				const updatedFiles = [...prev, ...newArr];
-				console.log("Обновленные файлы: ", updatedFiles); // Логируем обновленные файлы
-				return updatedFiles;
-			});
+			setSelectedFiles((prev) => [...prev, ...newArr]);
 		}
 	};
 
-	// Обработчик загрузки файлов
+	const { sendFile } = useFile();
+
 	const handleUpload = async () => {
 		if (selectedFiles.length === 0) {
 			setUploadStatus("Выберите файлы для загрузки.");
 			return;
 		}
-
 		const formData = new FormData();
-		// Добавление файлов в FormData
-		selectedFiles.forEach((item) => {
-			console.log("Добавляем файл в FormData: ", item); // Логируем каждый файл
-			formData.append("worker-card", item);
-		});
+		selectedFiles.forEach((item) => formData.append("worker-card", item));
+
+		// Выводим FormData в консоль для проверки
+		console.log(...formData);
 
 		try {
-			const response = await fetch("http://localhost:3000/users/worker-card", {
-				method: "POST",
-				body: formData,
-			});
-
-			const result = await response.json();
-
-			if (response.ok) {
-				setUploadStatus("Файл успешно загружен");
-			} else {
-				setUploadStatus(
-					`Ошибка при загрузке: ${result.message || "Неизвестная ошибка"}`,
-				);
-			}
-		} catch (error) {
+			await sendFile(formData);
+			setUploadStatus("Файл успешно отправлен");
+		} catch {
 			setUploadStatus("Ошибка при загрузке файлов.");
+			console.log("km");
 		}
 	};
 
-	// Обработчик удаления файла
 	const handleDltFile = (fileId: number) => {
-		setSelectedFiles((prev) => prev.filter((item) => item.id !== fileId));
+		setSelectedFiles((item) => item.filter((item) => item.id !== fileId));
 	};
 
-	// Загружаем файлы, если approve = true
 	useEffect(() => {
 		if (approve) {
 			handleUpload();
+			console.log(`km2`);
+		} else {
+			console.log(approve);
 		}
 	}, [approve]);
-
 	return (
 		<div className="worker-content worker__experience">
 			<TitleSection title="Профессиональная информация" />
@@ -124,6 +106,80 @@ const WorkerExperience: React.FC<WorkerExperienceProps> = ({
 			/>
 
 			<div className="workerExperience__card-content">
+				<div className="workerExperience-card">
+					<h3 className="work__experience-title">Текущая работа</h3>
+					<Form
+						inputs={[
+							{
+								name: "currJobStatus",
+								value: validWorker?.currJobStatus,
+								classname: "workerPersonal-inp",
+								disabled: true,
+								label: "Статус работы",
+							},
+							{
+								name: "currJob",
+								value: validWorker?.currJob,
+								classname: "workerPersonal-inp",
+								disabled: true,
+								label: "Место работы",
+							},
+							{
+								name: "currJobPosition",
+								classname: "workerPersonal-inp",
+								value: validWorker?.currJobPosition,
+								disabled: true,
+								label: "Должность",
+							},
+							{
+								name: "workStatus",
+								value: validWorker?.currJobPeriod,
+								disabled: true,
+								classname: "workerPersonal-inp",
+								label: "Период",
+							},
+						]}
+						classname="workerPersonal-form workerExperience-form"
+					/>
+				</div>
+
+				<div className="workerExperience-card">
+					<h3 className="prevjob-title">Предыдущая работа</h3>
+					<Form
+						inputs={[
+							{
+								name: "PrevJobStatus",
+								value: validWorker?.prevJobStatus,
+								disabled: true,
+								classname: "workerPersonal-inp",
+								label: "Статус работы",
+							},
+							{
+								name: "prevJob",
+								value: validWorker?.prevJob,
+								classname: "workerPersonal-inp",
+								disabled: true,
+								label: "Место работы",
+							},
+							{
+								name: "prevJobPosition",
+								value: validWorker?.prevJobPositon,
+								disabled: true,
+								classname: "workerPersonal-inp",
+								label: "Должность",
+							},
+							{
+								name: "prevJobPeriod",
+								value: validWorker?.prevJobPeriod,
+								label: "Период",
+								classname: "workerPersonal-inp",
+								disabled: true,
+							},
+						]}
+						classname="workerPersonal-form workerExperience-form"
+					/>
+				</div>
+
 				{/* Загрузка файлов */}
 				<div className="file-upload-content">
 					<label htmlFor="fileInput" className="file-label">
@@ -141,19 +197,63 @@ const WorkerExperience: React.FC<WorkerExperienceProps> = ({
 						{selectedFiles?.map((item) => (
 							<div className="file-upload-list" key={item.id}>
 								<span className="file-upload-item">
-									{item.name}
+									{item.id}
 									<Button onClick={() => handleDltFile(item.id)}>
 										Удалить
 									</Button>
 								</span>
 							</div>
 						))}
-						<Button
-							onClick={handleUpload}
-							disabled={selectedFiles.length === 0}
-						>
-							Отправить
-						</Button>
+						<Button onClick={handleUpload}>Отправит2 ь</Button>
+					</div>
+					{/* Квалификация */}
+					<TitleSection title="Квалификация" />
+					<div className="file-upload-list-content ">
+						<label htmlFor="fileInput" className="file-label personaly-files">
+							+
+						</label>
+						<input
+							type="file"
+							id="fileInput"
+							className="file-input"
+							multiple
+							// onChange={handleFileChange}
+						/>
+						{/* {uploadStatus && <p>{uploadStatus}</p>} */}
+						{/* {selectedFiles?.map((item) => (
+							<div className="file-upload-list" key={item.id}>
+								<span className="file-upload-item">
+									{item.id}
+									<Button onClick={() => handleDltFile(item.id)}>
+										Удалить
+									</Button>
+								</span>
+							</div>
+						))} */}
+					</div>
+					{/* Личные документы */}
+					<TitleSection title="Личные документы " />
+					<div className="file-upload-list-content">
+						<label htmlFor="fileInput" className="file-label personaly-files">
+							+
+						</label>
+						<input
+							type="file"
+							id="fileInput"
+							className="file-input"
+							multiple
+							// onChange={handleFileChange}
+						/>
+						{/* {selectedFiles?.map((item) => (
+							<div className="file-upload-list" key={item.id}>
+								<span className="file-upload-item">
+									{item.id}
+									<Button onClick={() => handleDltFile(item.id)}>
+										Удалить
+									</Button>
+								</span>
+							</div>
+						))} */}
 					</div>
 				</div>
 			</div>
